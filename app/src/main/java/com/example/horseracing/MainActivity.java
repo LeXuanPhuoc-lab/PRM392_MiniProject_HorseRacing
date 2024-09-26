e4399120b3b369990ff2327c40a9200835678ecc
LeXuanPhuoc-Lab <stevenmarks147@gmail.com>
2024-09-26 14:08:54 +0700

    Enhance dashboard appearance

--- app/src/main/java/com/example/horseracing/MainActivity.java
+++ app/src/main/java/com/example/horseracing/MainActivity.java
@@ -31,9 +31,9 @@ public class MainActivity extends AppCompatActivity {
              @Override
              public void onClick(View view) {
                  Intent it = new Intent(MainActivity.this, DashboardActivity.class);
-                 it.putExtra("horseImageId", R.drawable.pikachu);
-                 it.putExtra("totalBet", 2);
-                 it.putExtra("totalWin", 2);
+                 it.putExtra("firstPlaceImg", R.drawable.pikachu);
+                 it.putExtra("secondPlaceImg", R.drawable.steve);
+                 it.putExtra("thirdPlaceImg", R.drawable.mario);
                  it.putExtra("result", 1); // WIN
 //                 it.putExtra("result", 0); // LOSE
                  it.putExtra("amount", 100d);
